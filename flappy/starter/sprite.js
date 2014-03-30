4c4985819712e3ecf89fad4a5ad0950660ae3b8b
Max Wihlborg <max.wihlborg@gmail.com>
2014-03-30 17:00:49 +0200

    Updated flappy starter files

--- flappy/starter/sprite.js
+++ flappy/starter/sprite.js
@@ -1,7 +1,8 @@
 var 
 
 s_bird,
-s_bg, s_fg,
+s_bg,
+s_fg,
 s_pipeNorth,
 s_pipeSouth,
 s_text,
@@ -14,10 +15,10 @@ s_numberB;
 
 function Sprite(img, x, y, width, height) {
 	this.img = img;
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
+	this.x = x*2;
+	this.y = y*2;
+	this.width = width*2;
+	this.height = height*2;
 };
 Sprite.prototype.draw = function(ctx, x, y) {
 	ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
