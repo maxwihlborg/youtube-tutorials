47dda5b98815c7c8672f100807555e650e5b32cc
Max Wihlborg <max.wihlborg@gmail.com>
2014-04-13 20:40:08 +0200

    Annotated flappy bird source

--- flappy/sprite.js
+++ flappy/sprite.js
@@ -1,5 +1,7 @@
 var 
 
+// Sprite vars //
+
 s_bird,
 s_bg,
 s_fg,
@@ -12,7 +14,15 @@ s_buttons,
 s_numberS,
 s_numberB;
 
-
+/**
+ * Simple sprite class
+ * 
+ * @param {Image}  img    spritesheet image
+ * @param {number} x      x-position in spritesheet
+ * @param {number} y      y-position in spritesheet
+ * @param {number} width  width of sprite 
+ * @param {number} height height of sprite
+ */
 function Sprite(img, x, y, width, height) {
 	this.img = img;
 	this.x = x*2;
@@ -20,11 +30,23 @@ function Sprite(img, x, y, width, height) {
 	this.width = width*2;
 	this.height = height*2;
 };
+/**
+ * Draw sprite ta canvas context
+ * 
+ * @param  {CanvasRenderingContext2D} ctx context used for drawing
+ * @param  {number} x   x-position on canvas to draw from
+ * @param  {number} y   y-position on canvas to draw from
+ */
 Sprite.prototype.draw = function(ctx, x, y) {
 	ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
 		x, y, this.width, this.height);
 };
 
+/**
+ * Initate all sprite
+ * 
+ * @param  {Image} img spritesheet image
+ */
 function initSprites(img) {
 
 	s_bird = [
@@ -34,7 +56,7 @@ function initSprites(img) {
 	];
 	
 	s_bg = new Sprite(img,   0, 0, 138, 114);
-	s_bg.color = "#70C5CF";
+	s_bg.color = "#70C5CF"; // save background color
 	s_fg = new Sprite(img, 138, 0, 112,  56);
 	
 	s_pipeNorth = new Sprite(img, 251, 0, 26, 200);
@@ -60,11 +82,21 @@ function initSprites(img) {
 	s_numberS = new Sprite(img, 0, 177, 6,  7);
 	s_numberB = new Sprite(img, 0, 188, 7, 10);
 
+	/**
+	 * Draw number to canvas
+	 * 
+	 * @param  {CanvasRenderingContext2D} ctx context used for drawing
+	 * @param  {number} x      x-position
+	 * @param  {number} y      y-position
+	 * @param  {number} num    number to draw
+	 * @param  {number} center center to offset from
+	 * @param  {number} offset padd text to draw right to left
+	 */
 	s_numberS.draw = s_numberB.draw = function(ctx, x, y, num, center, offset) {
 		num = num.toString();
 
 		var step = this.width + 2;
-
+		
 		if (center) {
 			x = center - (num.length*step-2)/2;
 		}
